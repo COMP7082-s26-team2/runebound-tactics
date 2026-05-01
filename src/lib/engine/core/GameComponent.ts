c37e1c14b7cff062c99cee8115f10ad24b888d1b
Kurt Milan <kdmilan724@gmail.com>
2026-05-01 12:30:54 -0700

    BCOMP-6 added pre and post draw for game component

--- src/lib/engine/core/GameComponent.ts
+++ src/lib/engine/core/GameComponent.ts
@@ -5,6 +5,10 @@ export interface GameComponent {
 
     draw?(ctx: CanvasRenderingContext2D, alpha: number): void;
 
+    preDraw?(ctx: CanvasRenderingContext2D, alpha: number): void;
+
+    postDraw?(ctx: CanvasRenderingContext2D, alpha: number): void;
+
     destroy?(): void;
 
     zIndex?: number; // optional rendering order
