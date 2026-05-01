88749716104474e2c5d855e436e81918aad51ec0
Exarch-Eden <rikysmile33@gmail.com>
2026-04-30 23:09:52 -0700

    Updated InputSystem class
    Added key states and counter private class variables

--- src/lib/engine/InputSystem.ts
+++ src/lib/engine/InputSystem.ts
@@ -1,4 +1,16 @@
 class InputSystem {
+    // Key states
+    // KeyboardEvent.code values
+    private held = new Set(); // currently held down
+    private justPressed = new Set(); // pressed this frame (cleared on update)
+    private justReleased = new Set(); // released this frame (cleared on update)
+
+    // Counters
+    // incremented each time a key is pressed
+    // used to resolve two conflicting actions based on which was pressed last
+    private pressCounter = 0;
+    private pressOrder = new Map();
+
     // Mouse state
     private mouseX = 0;
     private mouseY = 0;
