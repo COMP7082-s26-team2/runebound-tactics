bfb9d0195bc348eeb15d28525f47c47dc72279ff
Exarch-Eden <rikysmile33@gmail.com>
2026-04-30 23:07:43 -0700

    Created InputSystem class

---
+++ src/lib/engine/InputSystem.ts
@@ -0,0 +1,10 @@
+class InputSystem {
+    // Mouse state
+    private mouseX = 0;
+    private mouseY = 0;
+    private mouseHeld = new Set();
+    private mouseJustPressed = new Set();
+    private mouseJustReleased = new Set();
+}
+
+export default InputSystem;
