c3ba92057d8263d150ec28ce44c2e7cc89b764c6
Exarch-Eden <rikysmile33@gmail.com>
2026-05-02 16:15:01 -0700

    Updated TurnSystem class
    Added private "round" and "eventBus" class fields

--- src/lib/engine/TurnSystem.ts
+++ src/lib/engine/TurnSystem.ts
@@ -1,3 +1,5 @@
+import EventBus from "./EventBus";
+
 interface Participant {
     id: string;
 }
@@ -5,10 +7,12 @@ interface Participant {
 class TurnSystem {
     private _participants: Participant[] = [];
     private _index = 0;
+    private _round = 0;
     private _started = false;
     private _loop: boolean | null = null;
     private _acted = new Set<string>();
     private _skipped = new Set<string>();
+    private _eventBus: EventBus | null = null;
 
     get activeParticipant() {
         return this._participants[this._index] ?? null;
