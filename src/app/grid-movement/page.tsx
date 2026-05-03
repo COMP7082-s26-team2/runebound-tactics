902ca2759e85691daeec8e3471602fb4227311f5
kurtbot <56173650+kurtbot@users.noreply.github.com>
2026-05-03 02:16:22 -0700

    BCOMP-6 updated grid movement to make input system independent

--- src/app/grid-movement/page.tsx
+++ src/app/grid-movement/page.tsx
@@ -2,7 +2,9 @@ import { GridMovementCanvas } from "@/components/";
 
 export default function Page() {
     return (
-        <main className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <main className="flex flex-row flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <GridMovementCanvas debug={true} />
+            <div className="w-8" />
             <GridMovementCanvas debug={true} />
         </main>
     );
