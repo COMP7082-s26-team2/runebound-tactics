19da69ab9bcfc2d13e1b9f9646b26906803a9801
Exarch-Eden <rikysmile33@gmail.com>
2026-05-17 20:34:11 -0700

    Created CombatSystem class

---
+++ src/lib/game/systems/CombatSystem.ts
@@ -0,0 +1,26 @@
+import { EntityId, World } from "@/lib/engine";
+
+type AttackResult = {
+    damage: number;
+    defenderDied: boolean;
+    newDefenderHp: number;
+};
+
+class CombatSystem {
+    constructor(private readonly _world: World) {}
+
+    resolveAttack(
+        attackerId: EntityId,
+        targetid: EntityId,
+    ): AttackResult | null {}
+
+    applyAttackResult(
+        result: AttackResult,
+        _attackerId: EntityId,
+        targetId: EntityId,
+    ): void {}
+
+    computeAttackable(entityId: EntityId): Set<EntityId> {}
+}
+
+export default CombatSystem;
