9edbe7788565e7defd3f4c43ff89a433fbe3423d
kurtbot <56173650+kurtbot@users.noreply.github.com>
2026-05-19 01:55:14 -0700

    BCOMP-63 WIP added simple UI element for lobby creation

---
+++ client/src/app/multiplayer/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { BackButton } from "@/components/ui/BackButton";
+import { Button } from "@/components/ui/Button";
+import { Modal } from "@/components/ui/Modal";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+function MultiplayerPage() {
+    const router = useRouter();
+    const [createState, setCreateState] = useState(false);
+
+    const handleCreate = () => {
+        console.log(`Create Lobby Modal Show`);
+        setCreateState(!createState);
+    };
+
+    const handleJoin = () => {
+        console.log(`Join Lobby Modal Show`);
+    };
+
+    const handleModalClose = () => {
+        setCreateState(!createState);
+    };
+
+    const handleModalCreate = () => {
+        router.push("/lobby", {});
+    };
+
+    return (
+        <>
+            <div className="min-h-screen bg-gray-900 flex justify-center flex-col gap-4 p-4">
+                <Button onClick={handleCreate}>Create Lobby</Button>
+                <Button onClick={handleJoin}>Join Lobby</Button>
+                <BackButton router={router}>Back</BackButton>
+            </div>
+            <div>
+                <Modal isOpen={createState} onClose={handleModalClose}>
+                    <Button onClick={handleModalCreate}>Create Lobby</Button>
+                </Modal>
+            </div>
+        </>
+    );
+}
+
+export default MultiplayerPage;
