bd7d606048a7ca05e3211b74a030669b55c1b06f
Li Yan Tong <liyan.tong@gmail.com>
2026-05-27 11:25:52 -0700

    [BCOMP-81] Try serving as webserver instead of static site

--- client/src/lib/prisma.ts
+++ client/src/lib/prisma.ts
@@ -1,36 +1,36 @@
-// import { PrismaClient } from "@prisma/client";
-// import { PrismaPg } from "@prisma/adapter-pg";
-// import { Pool } from "pg";
-// import "dotenv/config";
-// import { log } from "console";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import "dotenv/config";
+import { log } from "console";
 
-// const prismaClientSingleton = () => {
-//     log(process.env.DATABASE_URL);
+const prismaClientSingleton = () => {
+    log(process.env.DATABASE_URL);
 
-//     const pool = new Pool({
-//         connectionString: process.env.DATABASE_URL,
-//     });
-//     const adapter = new PrismaPg(pool);
-//     return new PrismaClient({ adapter });
-// };
+    const pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+    });
+    const adapter = new PrismaPg(pool);
+    return new PrismaClient({ adapter });
+};
 
-// declare const globalThis: {
-//     prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
-// } & typeof global;
+declare const globalThis: {
+    prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
+} & typeof global;
 
-// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-// export default prisma;
+export default prisma;
 
-// if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
 
-// /**
-//  * BIGINT SERIALIZATION FIX
-//  * ------------------------
-//  * Since PostgreSQL primary keys use BIGINT, Prisma returns them as JavaScript BigInt.
-//  * JSON.stringify() fails by default on BigInt.
-//  * This adds a global toJSON method to BigInt to convert them to strings automatically.
-//  */
-// (BigInt.prototype as any).toJSON = function () {
-//     return this.toString();
-// };
+/**
+ * BIGINT SERIALIZATION FIX
+ * ------------------------
+ * Since PostgreSQL primary keys use BIGINT, Prisma returns them as JavaScript BigInt.
+ * JSON.stringify() fails by default on BigInt.
+ * This adds a global toJSON method to BigInt to convert them to strings automatically.
+ */
+(BigInt.prototype as any).toJSON = function () {
+    return this.toString();
+};
