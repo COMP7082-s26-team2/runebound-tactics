f2200415ff525a9ea249c6b3d4445d357e45b396
Li Yan Tong <liyan.tong@gmail.com>
2026-05-27 09:21:02 -0700

    [BCOMP-81] Update package install and client lobby room search
    Export no longer needed for next.config.ts because we have a server
    delivering the on-demmand room

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     transpilePackages: ["@runebound-tactics/shared"],
-    output: "export",      // Forces Next.js to generate flat HTML/CSS/JS files
+    //output: "export",      // Forces Next.js to generate flat HTML/CSS/JS files
     images: {
         unoptimized: true, // Required because static exports don't have an image optimization server
     },
