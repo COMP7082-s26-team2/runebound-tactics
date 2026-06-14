53eb625ebb0bf069db1f65446f378e0c9a9b34b2
Li Yan Tong <liyan.tong@gmail.com>
2026-06-14 14:12:25 -0700

    [BCOMP-56] Initialize Card class

---
+++ shared/src/schemas/Card.ts
@@ -0,0 +1,26 @@
+import { Schema, type, ArraySchema } from "@colyseus/schema";
+
+export enum CardType {
+  CARD_STATUS_EFFECT = 'status',
+  CARD_SPELL_EFFECT = 'spell'
+}
+
+export class Card extends Schema {
+    @type("string") name: string;
+    @type("string") card_type: CardType;
+    @type("number") gold_cost: number;
+    @type("boolean") is_reaction: boolean = false;
+
+    constructor(name: string, card_type: CardType, gold_cost: number, is_reaction: boolean) {
+        super();
+        this.name = name;
+        this.card_type = card_type;
+        this.gold_cost = gold_cost;
+        this.is_reaction = is_reaction;
+    }
+}
+
+export class DeckState extends Schema {
+    // We use ArraySchema<Card> so Colyseus can track mutations inside the array
+    @type([ Card ]) cards = new ArraySchema<Card>();
+}
